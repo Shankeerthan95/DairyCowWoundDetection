ebf4a0675235d93fa010f0bc53a6960e9c561207
Shankeerthan Kasilingam <33720251+Shankeerthan95@users.noreply.github.com>
2017-11-23 10:18:25 +0530

    Initial commit

---
+++ src/com/shankeerthan/Main.java
@@ -0,0 +1,18 @@
+package com.shankeerthan;
+
+import javafx.application.Application;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        //Border Pane is root for this software
+        BorderPane root =new BorderPane();
+
+    }
+
+    private void addNodesToRoot(BorderPane root){
+
+    }
+}
