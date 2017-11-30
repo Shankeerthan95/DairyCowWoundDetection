d92c78f7dfd56f57eb403acca454f677e8dfb24d
Ano <anojan010@gmail.com>
2017-12-01 01:08:54 +0530

    commit test

---
+++ src/main/java/com/shankeerthan/Values.java
@@ -0,0 +1,50 @@
+package com.shankeerthan;
+
+public class Values {
+    public static final  double IMAGE_POSITION_BOX_HEIGHT=40;
+    public static final double TOP_BAR_HEIGHT=20;
+    public final static double RIGHT_BAR_WIDTH=20;
+    public static final double DEFAULT_SCENE_HEIGHT=950;
+    public static final double DEFAULT_SCENE_WIDTH=1000;
+    public static final double DEFAULT_NAME_CANVAS_WIDTH=400;
+    public static final double DEFAULT_TOP_BAR_NODES_HEIGHT=20;
+    public static final double TOP_BOX_SPACING=0;
+    public static final double POSITION_SLIDER_OFFSET=20;
+    public static final double SIDE_BOX_SPACING=20;
+    public static final double SIDE_BOX_PADDING_TOP_BOTTOM=40;
+    public static final double SMALL_MENU_DX=10;
+    public static final double SMALL_MENU_DY=150;
+    public static final double SLIDER_DX=460;
+    public static final double SLIDER_DY=210;
+    public static  final double GRID_VGAP=20;
+    public static  final int CELSIUS =0;
+    public static final int FAHRENHEIt=1;
+    public static final int KELVIN=2;
+    /*
+    These number s sre used to Define Color palletes
+     */
+    public static final int FUSION=0;
+    public static final int IRONBOW_1=1;
+    public static final int IRONBOW_2=2;
+    public static final int RAINBOW=3;
+    public static final int RAIN=4;
+    public static final int SEPIA=5;
+    public static final int GLOWBOW=6;
+    public static final int WHITE_HOT=7;
+    public static final int BLACK_HOT=8;
+    public static final int ICE_AND_FIRE=9;
+    public static final int COLOR_1=10;
+    public static final int COLOR_2=11;
+    public static final int SIZE_RANGE_COLORS=200;
+
+    /*
+    aLL VALES IN CELCIOUS
+     */
+    public static  final String HIGH_TEM_SCALE_RANGE="35";
+    public static final String LOW_TEM_SCALE_RANGE="25";
+    public static final String LOW_TEM_REGION="28";
+    public static final String HIGH_TEM_REGION="32";
+
+    //public static final double BUTTON_FIT_WIDTH=24;
+    //public static final double BUTTON_FIT_HEIGHT=24;
+}
