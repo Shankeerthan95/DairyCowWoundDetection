640920988195406e16af44c35054a814c42364da
Ano <anojan010@gmail.com>
2017-12-03 02:36:14 +0530

    compareColor test is finished in Coloseparator class

--- src/test/java/com/ano/ColorSeparatorTest.java
+++ src/test/java/com/ano/ColorSeparatorTest.java
@@ -1,6 +1,7 @@
 package com.ano;
 
 import com.shankeerthan.ColorSeparator;
+import javafx.scene.paint.Color;
 import org.junit.Test;
 import org.junit.experimental.theories.suppliers.TestedOn;
 
@@ -18,6 +19,23 @@ public class ColorSeparatorTest {
         m.setAccessible(true);
         double distance=(double)m.invoke(colorSeparator,0,0,0,1,2,3);
         assertEquals(3.7416573867739413,distance,0);
+    }
+
+    @Test
+    public void getDistanceTestReal() throws Exception{
+        Class c = Class.forName("com.shankeerthan.ColorSeparator");
+        ColorSeparator colorSeparator = (ColorSeparator) c.newInstance();
+        Method m = c.getDeclaredMethod("getDistance", double.class, double.class, double.class, double.class, double.class, double.class);
+        m.setAccessible(true);
+        double distance=(double)m.invoke(colorSeparator,1,0,0,0,0,1);
+        assertEquals(1.414213562,distance,0.000000001);
+    }
+
+    @Test
+    public void compareColorsTest(){
+        boolean val = ColorSeparator.compareColors(Color.RED,Color.BLUE,2);
+        System.out.println(Color.BLUE.getRed());
+        assertTrue(val==true);
     }
 
 }
